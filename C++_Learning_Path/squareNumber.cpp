e00c2a7fd988c2400a71bedf26ab86ec7f9cfdca
Mert Kuzu <62839172+MertKuzu@users.noreply.github.com>
2021-11-11 01:16:36 +0300

    Add files via upload

---
+++ C++_Learning_Path/squareNumber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,pow;
+cout<<"Enter a number:";
+cin>>a;
+pow = a*a;
+cout<<"The number you entered="<<a<<" and square="<<pow<<"\n";
+system("PAUSE");
+return 0;	
+}
---
+++ C++_Learning_Path/sumNumbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,total;
+	cout<<"Toplamak istediginiz iki sayi girin:";
+	cin>>a>>b;
+	total = a+b;
+	cout<<"Toplam="<<total<<"\n";
+	system("PAUSE");
+	return 0;
+}
